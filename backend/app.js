6eb48e78f2c943c8d532858f9f3ab6131c5402e7
J1nu-Lo3 <jcezar61@yahoo.com>
2026-02-05 15:02:02 +0100

    connection établie avec mon cluster MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://J1nu-Lo3:ajFyfYpkV6F6g0Zs@cluster0.jh3i0gr.mongodb.net/test?retryWrites=true&w=majority",
+  )
+  .then(() => console.log("Connexion à MongoDB Atlas réussie !"))
+  .catch((err) => console.error("Connexion à MongoDB Atlas échouée !", err));
 
 app.use((req, res, next) => {
   console.log("Requête reçue !");
