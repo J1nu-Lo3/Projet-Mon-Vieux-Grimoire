02c43bb34d48aa47053d1913a3296353dbc05aba
J1nu-Lo3 <jcezar61@yahoo.com>
2026-02-09 15:36:14 +0100

    fini d'adapter le code du cours dans ce projet (things -> books)

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const app = express();
+const path = require("path");
+const bookRoutes = require("./routes/books");
+const userRoutes = require("./routes/user");
 
 mongoose
   .connect(
     "mongodb+srv://J1nu-Lo3:ajFyfYpkV6F6g0Zs@cluster0.jh3i0gr.mongodb.net/test?retryWrites=true&w=majority",
   )
   .then(() => console.log("Connexion à MongoDB Atlas réussie !"))
-  .catch((err) => console.error("Connexion à MongoDB Atlas échouée !", err));
+  .catch((error) =>
+    console.error("Connexion à MongoDB Atlas échouée !", error),
+  );
+
+const app = express();
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-requested-With, Content, Accept, Content-Type, Authorization",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(express.json());
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use("/api/books", bookRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
